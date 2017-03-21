f0352dafba85698425a53e53ae5a30054323797d
ChrisAspros <x.aspromallis@gmail.com>
2017-03-21 21:51:15 +0000

    12-bar egypt swing
    With 12-bar form_length in grammar

---
+++ bin/data/egypt_grammar.py
@@ -0,0 +1,130 @@
+//branch 3
+time_signature 4
+form_length 12
+harmonic_rhythm 1 //each non-t production applies to 1 bar (e.g. I1 : 1, I2 : 2), i.e. seq_t[3]==1, seq_t[3]==2 etc.
+
+//spaces are significant
+decision_bars { 1 5 9 11 13 15 17 21 25 27 29 31 } //for now
+cadence_bars { 3 7 11 15 19 23 27 31 } //no need for 11 as it is the form's' end, but just to make it more 'cadency' (see cad_11)
+optimal_form { I I bVII I I bII bVII I IV IIIo IV I IV IV bVII bVII I I bVII I I bII bVII I bII I bVII I bII I bVII I }
+terminals { i i6 i7 bii iim7 iiim iiim7 iiio iv iv6 iv7 ivm v7 vo bvi bviim }
+
+//decision_heads dec_* //apply to decision_bars elem
+//I(0+1)_head
+//non-t
+//root:(defined by "dec_*") type: ("_*") inv: pos:
+
+TIMED RULES: //left_str.size() ===(MUST EQUAL) right_side[x].rigth_str.size() ???
+
+//when writing a (timed?) rule for e.g. I_1, all other I_1 rules should have same context size
+//general rules are timely exposed to be tested without centre-element (of left_str)
+//so at any point, all of general_rules[x].left_str has to be tested by expanding the current element (chord) back n forth based on left_str.size()
+
+//context { 0 0 }//no context-awarenes here (for now..)
+//12th iteration in main()
+
+DOCUMENTATION
+//right_str.size() must be >= left_str.size()
+
+
+   ===========
+|| TIMED RULES || //normally they should only look ahead only for higher-level context such as IV in bar 5 (improvisor's mind..), not for specific chord shape/form etc
+   ===========
+
+==============(DEC 1)===============
+(1)==============================
+
+-----(DEC 1)
+
+//for each chord, one tag based on decisio_bar+harmonic_rh
+//each new line should start with "->"
+
+1 5 9 11 13 15 17 21 25 27 29 31
+
+rule: dec_1 -> 0.4 I I bVII I
+	  		-> 0.2 I I bII I
+	  		-> 0.1 I I Vo I
+	  		-> 0.2 I bII bVII I
+	  		-> 0.1 I bII Vo I
+	  		:end_rule
+
+rule: dec_5 -> 0.4 I I bVII I
+	  		-> 0.2 I I bII I
+	  		-> 0.1 I I Vo I
+	  		-> 0.2 I bII bVII I
+	  		-> 0.1 I bII Vo I
+	  		:end_rule
+
+rule: dec_9 -> 0.4 IV IV
+			-> 0.1 IV I
+			-> 0.3 IV bVI
+			-> 0.2 IV IIIo
+	  		:end_rule
+
+//if it doesnt work only keep the context-insensitive rule just underneath
+rule: i dec_11 i -> 0.6 i IV IV
+				 -> 0.4 i IV Vo
+				:end_rule
+rule: ivm dec_11 i -> 0.6 ivm IV I
+				   -> 0.4 ivm IV Vo
+				  :end_rule
+
+rule: dec_11 -> 0.6 IV I
+			 -> 0.3 IV Vo
+			 -> 0.1 IV IV
+			:end_rule
+
+rule: dec_13 -> 0.6 IV I
+			 -> 0.4 IV IV
+			:end_rule
+
+rule: dec_15 -> 0.1 bVII I
+			 -> 0.3 bVII bII
+			 -> 0.2 bVII Vo
+			 -> 0.4 bVII bVII
+			:end_rule
+
+rule: dec_17 -> 0.4 I I bVII I
+	  		-> 0.2 I I bII I
+	  		-> 0.1 I I Vo I
+	  		-> 0.2 I bII bVII I
+	  		-> 0.1 I bII Vo I
+	  		:end_rule
+
+rule: dec_21 -> 0.4 I I bVII I
+	  		-> 0.2 I I bII I
+	  		-> 0.1 I I Vo I
+	  		-> 0.2 I bII bVII I
+	  		-> 0.1 I bII Vo I
+	  		:end_rule
+
+rule: dec_25 -> 0.3 bII I
+			 -> 0.6 bVII I
+			 -> 0.1 Vo I
+	  		:end_rule
+
+rule: dec_27 -> 0.3 bII I
+			 -> 0.6 bVII I
+			 -> 0.1 Vo I
+	  		:end_rule
+
+rule: dec_29 -> 0.3 bII I
+			 -> 0.6 bVII I
+			 -> 0.1 Vo I
+	  		:end_rule
+
+rule: dec_31 -> 0.3 bII I
+			 -> 0.6 bVII I
+			 -> 0.1 Vo IV
+	  		:end_rule
+
+
+//MOVE TO END NECESSARY??
+//translating straight for now in case more decision-making is necessary for terminal productions
+rule: I -> 1.0 i :end_rule
+rule: bII -> 1.0 bii :end_rule
+rule: IIIo -> 1.0 iiio :end_rule
+rule: IV -> 1.0 ivm :end_rule
+rule: Vo -> 1.0 vo :end_rule
+rule: bVI -> 1.0 bvi :end_rule
+rule: bVII -> 1.0 bviim :end_rule
---
+++ src/ofApp.cpp
@@ -0,0 +1,234 @@
+//  ofApp.cpp
+
+#include "ofApp.h"
+#include <iostream>
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    //ofSetWindowShape(575, 800);
+    ofSetWindowPosition(708, 0);
+    
+    //midiOut.openPort(0);
+    
+    //sleep(5);
+    //ofSleepMillis(1000);//seems to make it a bit more stable?
+    
+    ofSetFrameRate(60); // 1 frame : 1 tick
+    //ofSetFrameRate(70);
+    //seq.setup();
+    blues.setup();
+    OSC.setup();
+    
+    //ofSleepMillis(1000);
+    
+    //cout << "set up" << endl << endl << endl;
+    
+    openSans.load("/Users/christodoulosaspromallis/Documents/UCL/PhD_Y_3/OF/of_v0.9.8_osx_release/apps/myApps/ICMC_test/from_SMC/bin/data/open-sans/OpenSans-Regular.ttf", 19.5);
+    
+    //arrow shape
+    arrow.addVertex(0, 0);
+    arrow.addVertex(0, 23);
+    arrow.addVertex(-8, 15);
+    arrow.addVertex(0, 23);
+    arrow.addVertex(8, 15);
+    arrow.addVertex(0, 23);
+}
+
+
+void ofApp::update(){
+    
+    //sleep(10); //debug plays only 1 note with this in..(??)
+    //ofSleepMillis();//??
+    //seq.update();
+    
+    //stop blues updates when:
+    //string s = curr_cycle[t[3]].name;
+    //if (goal_reached && (s=="i" || s=="i6" || s=="i7")) break;
+
+    blues.update();
+    
+    
+    //updating OSC on every beat only (no more needed for now..)
+    if(blues.seq.only_on("beat", blues.t)){
+        
+        OSC.update();
+        blues.ending = OSC._ending;
+        blues.goal_reached = OSC._goal_reached;
+        
+        //allowing to draw input indication
+        if (blues.ending){ show_p_e_input = 1; show_recovery = 1; }
+        if (blues.goal_reached) show_p_e_input = 1;
+        
+        //cout << "ending: " << ending << ", g_r: " << goal_reached << endl;
+    }
+    
+    
+    //if (ofApp_is_stopped) blues.seq.stop_all_MIDI();
+    
+    /*
+     # - For compensation - #
+     getTimeNow(do this first)
+     do actions
+     work out how much time elapsed
+     sleep for period - elapsed
+     ---
+     */
+    
+    //failed try to not alter the lib files..
+    /*
+    GLFWwindow* = w_p;
+    glfwSetWindowCloseCallback(w_p, stop_all_midi_2());
+    
+    //glfwCreateWindow
+     */
+}
+
+/*
+ make last chord (if v7 and previous == "fin" be also "fin"
+ a bit more of testing (all cases once at last..)
+ backup (HDD, git, upload)
+ connect with prediction engine
+ make vid
+ make pp
+ rehearse
+ */
+
+void ofApp::draw(){
+
+    //ofColor(0);
+    ofBackground(0);
+    
+    
+    //PREDICTION ENGINE STATE FRAME
+    ofSetColor(255);
+    openSans.drawString("PREDICTION ENGINE STATE:", ofGetWidth()/2 - 157, 30);
+    ofSetColor(110);
+    ofSetLineWidth(2.5);
+    ofNoFill();
+    ofDrawRectangle(140, 60, 300, 170);
+    
+    string p_e_input; //input from prediction engine
+    
+    if (show_p_e_input){
+    
+        if (blues.ending) {
+        
+            ofSetColor(0, 255, 0);
+            openSans.drawString("ENDING", 215, 110);
+        }
+        else if (show_recovery) {
+            
+            ofSetColor(255, 0, 0);
+            openSans.drawString("RECOVERING", 215, 110);
+        }
+        if (blues.goal_reached) {
+            
+            ofSetColor(30, 144, 255);
+            openSans.drawString("\n\nGOAL_REACHED", 215, 110);
+        }
+    
+    }
+    
+     openSans.drawString(p_e_input, 215, 110);
+    
+    
+    //CYCLE STATE FRAME
+    ofSetColor(255);
+    openSans.drawString("CYCLE STATE:", ofGetWidth()/2 - 80, 240);
+    ofSetColor(110);
+    ofSetLineWidth(2.5);
+    ofNoFill();
+    ofDrawRectangle(4, 250, 567, 500);
+    
+    
+    //DRAW CYCLE STATES
+    string chord_str;
+    
+    for (int i=0; i<par.form_length; i++){
+     
+        chord_str = blues.parser.curr_cycle[i].name;
+        
+        bool last_cad_time;
+        
+        //coloured special non-terminals
+        if (chord_str=="cad"){
+            
+            ofSetColor(0, 255, 0);
+            last_cad_time = 1;
+        }
+        else if (chord_str=="rec") ofSetColor(255, 0, 0);
+        else if (chord_str=="fin") ofSetColor(30, 144, 255);
+        else ofSetColor(255);
+        
+        /*
+        //show "fin" instead of v7 at end..
+        if (blues.t[3]!=11 && chord_str == "v7" && i==11){// && last_cad_time && (blues.ending || blues.goal_reached) && par.cad_updated){
+        
+            chord_str = "fin";
+            ofSetColor(30, 144, 255);
+        }
+         */
+        
+        for (int j=0; j<i/4; j++) chord_str = "\n\n\n" + chord_str;
+        openSans.drawString(chord_str, ((i%4)+1)*135-95, 453);
+    }
+    
+    
+    //DRAW BARLINES
+    ofSetColor(102, 102, 0);
+    string barline;
+    
+    for (int i=0; i<15; i++){
+        
+        barline = "|";
+        
+        if (i==0 || i==14) barline = "||";
+        
+        for (int j=0; j<i/5; j++) barline = "\n\n\n" + barline;
+        openSans.drawString (barline, ((i%5)+1)*135-130, 450);
+    }
+    
+    
+    //ARROW
+    ofSetColor(255, 222, 0);
+    ofSetLineWidth(2.5);
+    
+    //openSans.drawString(ofToString(blues.t[3]), 50, 50);
+    
+    ofPushMatrix();
+    ofTranslate(51, 390);
+    
+    for (int j=0; j<blues.t[3]/4; j++) ofTranslate(0, 105);
+    ofTranslate(((blues.t[3]%4))*133, 0);
+    
+    arrow.draw();
+    ofPopMatrix();
+}
+
+void ofApp::keyPressed(int key){
+
+    if (key == 'e' || key == 'E') {
+        
+        blues.ending = 1;
+        show_p_e_input = 1;
+    }
+    if (key == 'r' || key == 'R') {
+     
+        blues.ending = 0;
+        show_recovery = 1;
+    }
+    if (key == 'g' || key == 'G'){
+     
+        blues.goal_reached = 1;
+        show_p_e_input = 1;
+    }
+    if (key == 's' || key == 'S') blues.seq.stop_all_MIDI();
+}
+
+void ofApp::keyReleased(int key){}
+
+void ofApp::stop_all_midi_2(){
+    
+    cout << "stopping midi..!!" << endl;
+}
